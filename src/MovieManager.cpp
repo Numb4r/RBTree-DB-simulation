d3da5bdf6dd4a0b88363a7389b5ed75745b9c2f6
Numb4r <yuridimitre@protonmail.com>
2021-02-18 18:52:51 -0300

    Criado MovieManager

---
+++ src/MovieManager.cpp
@@ -0,0 +1,91 @@
+#include "MovieManager.hpp"
+
+void Database::makeMovieTree(const char* fileName){
+    const char* token="::";
+    RBTree<Movie> rb;
+    std::ifstream file{fileName};
+    if(!file){
+        std::cerr<<"[ERRO] Erro ao abrir o arquivo"<<std::endl;
+        exit(-1);
+    }
+    std::string s;
+    int id;
+    char* name;
+    while (std::getline(file,s))
+    {
+        
+        char str[strlen(s.c_str()+1)];
+        strcpy(str,s.c_str());
+        char *pch = strtok(str,token);
+        id = atoi(pch);
+        pch = strtok(NULL,token);
+        name = new char[strlen(pch)+1];
+        strcpy(name,pch);
+        this->movieTree.insert(Movie(id,name));
+    }
+    file.close();
+}
+
+void Database::makeUserTree(const char* fileName){
+    
+    std::list<Movie> listMovies;
+    
+    FILE * file;
+    if(!(file = fopen(fileName,"r"))){
+        std::cerr<<"[ERRO] Erro ao abrir o arquivo"<<std::endl;
+        exit(-1);
+        
+    }
+    int userId{};
+    int movieId{};
+    
+    int dumb,dumb2;
+    char line[20];
+    userTree.insert(User(1));
+    while (!feof(file))
+    {
+        fscanf(file,"%d::%d::%d::%d",&userId,&movieId,&dumb,&dumb2);
+
+        User* us = userTree.search(User(userId),[](User v1,User v2){
+            return v1.ID == v2.ID;
+        });
+        // if (!us)
+        // {
+            // std::cout<<userId<<std::endl;
+            userTree.insert(User(userId));
+            // us = userTree.search(User(userId),[](User v1,User v2){
+            //     return v1.ID == v2.ID;
+            // });            
+        // }
+        /*
+        As linhas abaixo tornam o codigo extremamente lento
+        Nao e minha culpa, o algoritimo se comporta dessa forma
+        */
+       /*
+       TODO:
+       Fazer teste de tempo para:
+       leitura apenas dos filmes (escrita sequencial)
+       leitura apenas dos usuarios(escrita sequencial)
+       leitura apenas dos usuarios(escrita randomica com busca)
+       leitura apenas dos usuarios com criacao de lista dos filmes(escrita randomica com busca)
+       */
+
+        // Movie *movie = movieTree.search(Movie(movieId,""),[](Movie v1,Movie v2){
+        //         return v1.ID == v2.ID;
+        // });
+        // if(movie){
+        //     us->moviesWatched.push_back(*movie);
+        // }
+        
+    }
+    fclose(file);
+}
+std::ostream& operator<<(std::ostream& os,const Movie &m){
+    os << m.ID<<" / "<<m.name;
+    return os;
+}
+
+std::ostream& operator<<(std::ostream& os,const User &u){
+    os<<u.ID;
+    return os;
+}
---
+++ src/MovieManager.hpp
@@ -0,0 +1,86 @@
+#ifndef __MOVIEMANAGER__
+#define __MOVIEMANAGER__
+#include "RBTree.hpp"
+#include "RBTree.cpp"
+#include <cstring>
+#include <list>
+#include <fstream>
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/*                                                      Movie                                                              */
+struct Movie{
+    int ID;
+    char* name;
+    bool operator>(Movie obj){
+        return this->ID > obj.ID;
+    }
+    bool operator<(Movie obj){
+        return this->ID < obj.ID;
+    }
+    bool operator==(Movie obj){
+        return (this->ID == obj.ID);
+    }
+    
+    bool operator!=(Movie obj){
+        return(this->ID != obj.ID );
+    }
+    Movie()=default;
+    Movie(int id,char* name):ID(id),name(name){
+    }
+    Movie(int id):ID(ID),name(""){
+        
+    }
+    friend std::ostream& operator<<(std::ostream& os,const Movie &m);
+};
+
+
+/*                                                      Movie                                                              */
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+/*                                                      User                                                               */
+
+struct User{
+    int ID;
+    
+    std::list<Movie> moviesWatched;
+    bool operator>(User obj){
+        return this->ID > obj.ID;
+    }
+    bool operator<(User obj){
+        return this->ID < obj.ID;
+    }
+    bool operator==(User obj){
+        return this->ID == obj.ID;
+    }
+    
+    bool operator!=(User obj){
+        return this->ID != obj.ID;
+    }
+    User(int ID):ID(ID){}
+    User(int ID,std::list<Movie> moviesWatched):ID(ID),moviesWatched(moviesWatched){}
+    User()=default;
+
+};
+
+
+
+
+/*                                                      User                                                               */
+/*/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
+
+
+
+
+class Database
+{
+private:
+    
+    
+public:
+    RBTree<User> userTree;
+    RBTree<Movie> movieTree;
+    void makeUserTree(const char* fileName);
+    void makeMovieTree(const char* fileName);
+};
+
+#endif

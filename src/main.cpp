79e1dd9da1dae5584087109ad9465ccc9f05c90c
Numb4r <yuridimitre@protonmail.com>
2021-02-20 20:01:47 -0300

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -10,18 +10,18 @@ int main(int argc, char const *argv[])
     Database db;
     srand(time(nullptr));
     db.makeMovieTree(MOVIESFILE);
-    Movie *aux;
-    for (int i = 0; i < 100000; i++)
-    {
-        auto p = 1+ rand()%3951;
-        aux = db.movieTree.search(Movie(p),[](Movie v1,Movie v2){
-            return v1 == v2;
-        });
-    }
+    // Movie *aux;
+    // for (int i = 0; i < 100000; i++)
+    // {
+    //     auto p = 1+ rand()%3951;
+    //     aux = db.movieTree.search(Movie(p),[](Movie v1,Movie v2){
+    //         return v1 == v2;
+    //     });
+    // }
     int insertUser;
-    std::cout<<"Deseja inserir usuarios? Esse processo ira demorar uma quantidade enorme de tempo (1-Sim/2-Nao) ";
+    std::cout<<"Deseja inserir usuarios? Esse processo ira demorar uma quantidade razoavel de tempo (1-Sim/2-Nao) ";
     std::cin>>insertUser;
-    if(insertUser == 1 ) db.makeUserTree(USERFILE);
-    
+    if(insertUser == 1 )
+        db.makeUserTree(USERFILE);
     return 0;
 }

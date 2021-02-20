5c8b2376715b942c6ae5fcfcec3e2a5e1179a104
Numb4r <yuridimitre@protonmail.com>
2021-02-20 19:50:10 -0300

    Reescrito codigo de leitura do arquivo do usuario.Houve um ganho expresivo de tempo

--- src/Database.cpp
+++ src/Database.cpp
@@ -30,44 +30,40 @@ void Database::makeUserTree(const char* fileName){
     
     std::list<Movie> listMovies;
     
-    FILE * file;
-    if(!(file = fopen(fileName,"r"))){
-        std::cerr<<"[ERRO] Erro ao abrir o arquivo"<<std::endl;
-        exit(-1);
-        
-    }
+    const char* token = "::";
+    std::ifstream file{fileName};
     int userId{};
     int movieId{};
-    
-    int dumb,dumb2;
-    char line[20];
-    userTree.insert(User(1));
     bool userNotFound{false};
-    while (!feof(file))
+    std::string s;
+    while (std::getline(file,s))
     {
-        fscanf(file,"%d::%d::%d::%d",&userId,&movieId,&dumb,&dumb2);
-
-        User* us = userTree.search(User(userId));
-
-        if (!us)
-        {
+        
+        char str[strlen(s.c_str()+1)];
+        strcpy(str,s.c_str());
+        char *pch = strtok(str,token);
+        userId = atoi(pch);
+        pch = strtok(NULL,token);
+        movieId = atoi(pch);
+        User* us = userTree.search(userId);
+        if(!us){
             userNotFound = true;
             us = new User(userId);
+        } 
+
+        Movie* m = movieTree.search(movieId);
+
+        if (m) us->moviesWatched.push_back(*m); // Copy
+
+        if (userNotFound){
+            userTree.insert(*us);
+            delete us;
+            userNotFound = false;
         }
         
-    
-        Movie *movie = movieTree.search(Movie(movieId));
-        if(movie){
-            us->moviesWatched.push_back(*movie); //Copy
-        }
-        if(userNotFound){
-            userTree.insert(*us); //Copy
-            userNotFound = false;
-            delete us;
-        }
         
     }
-    fclose(file);
+    file.close();
 }
 std::ostream& operator<<(std::ostream& os,const Movie &m){
     os << m.ID<<" / "<<m.name;
